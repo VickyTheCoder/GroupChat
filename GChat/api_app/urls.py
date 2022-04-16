2ced47667625c0462b0cd034bb8d4634a2690d25
vikram <vikram.the.coder@gmail.com>
2022-04-16 10:21:37 +0530

    endpoint for adding users

--- GChat/api_app/urls.py
+++ GChat/api_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView
+from .views import RegisterView, LoginView, UserListCreateView
 
 app_name='api_app'
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('login/', LoginView.as_view()),
+    path('users/', UserListCreateView.as_view()),
 ]
--- GChat/api_app/views.py
+++ GChat/api_app/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -12,24 +12,18 @@ class RegisterView(GenericAPIView):
 
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
-
         if serializer.is_valid():
             user = serializer.save()
             if request.data.get('is_admin_user','').lower() == "true":
                 user.is_superuser = True
-                
             user.save()
-
             auth_token = api.get_tokens_for_user(user)
             data = {'user': request.data, 'token': auth_token}
-            
             return Response(data, status=status.HTTP_201_CREATED)
-
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(GenericAPIView):
     serializer_class = LoginSerializer
-
     def post(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
@@ -38,5 +32,20 @@ class LoginView(GenericAPIView):
             serializer = UserSerializer(user)
             data = {'user': serializer.data, 'token': auth_token}
             return Response(data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+class UserListCreateView(ListCreateAPIView):
+    serializer_class = UserSerializer
+    def create(self, request, *args, **kwargs):
+        try:
+            serializer = self.get_serializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.save()
+                if request.data.get('is_admin_user','').lower() == "true":
+                    user.is_superuser = True
+                user.save()
+                data = {'message':'User created successfully' }
+                return Response(data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response(e, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
